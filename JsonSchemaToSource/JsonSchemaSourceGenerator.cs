9fc5aaa61dde2b8718654b3aff76994987376a90
David Lewis <dlewis@gsfsgroup.com>
2020-09-26 13:58:44 -0500

    3 Add your Implementation of ISourceGenerator.

---
+++ JsonSchemaToSource/JsonSchemaSourceGenerator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.CodeAnalysis;
+
+namespace JsonSchemaToSource
+{
+    public class JsonSchemaSourceGenerator : ISourceGenerator
+    {
+        public void Execute(SourceGeneratorContext context)
+        {
+        }
+
+        public void Initialize(InitializationContext context)
+        {
+            // No initializationRequired
+        }
+    }
+}
