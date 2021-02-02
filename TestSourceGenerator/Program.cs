b44362b038f6717ad2661005a455ccb3d625176d
David Lewis <dlewis@gsfsgroup.com>
2021-02-01 23:44:44 -0600

    updated Microsoft.CodeAnalysis deps and added to the test schema.

--- TestSourceGenerator/Program.cs
+++ TestSourceGenerator/Program.cs
@@ -7,8 +7,10 @@ namespace TestSourceGenerator
     {
         static void Main(string[] args)
         {
-            var x = new Payload();
-            var y = x.Dict;
+            var payload = new Payload();
+            var dict = payload.Dict;
+            payload.Name = "bob";
+
             Console.WriteLine("Types in this assembly:");
             foreach (Type t in typeof(Program).Assembly.GetTypes())
             {
